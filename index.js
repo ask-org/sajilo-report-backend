f66b7ffa36161fc810b3bacf21182511ce0de512
himalf <himal.fullel15@gmail.com>
2024-09-27 17:28:22 +0545

    report backend

---
+++ index.js
@@ -0,0 +1,13 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = 5000;
+app.listen(port, () => {
+  console.log(`app is running on ${port} port`);
+  console.log(`http://localhost:${port}`);
+});
+app.get("/", (req, res) => {
+  res.send(
+    "The Backend app is running for Creating the system for makeing report files"
+  );
+});
