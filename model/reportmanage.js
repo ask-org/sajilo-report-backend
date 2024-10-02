69e642123ab93f9c1c2fa56b030c55502cd0eb90
himalf <himal.fullel15@gmail.com>
2024-10-02 20:59:04 +0545

    supabase

--- model/reportmanage.js
+++ model/reportmanage.js
@@ -1,8 +1,10 @@
 const { createClient } = require("@supabase/supabase-js");
 const supabaseURL = process.env.SUPABASE_URL;
 const supabaseKEY = process.env.SUPABASE_KEY;
+console.log(supabaseURL, "urlof supabse");
+console.log(supabaseKEY, "key of supabase");
 const supabase = createClient(supabaseURL, supabaseKEY);
-console.log(supabase, "hello");
+// console.log(supabase, "hello");
 const getReports = async () => {
   const { data, error } = await supabase.from("reportgenerator").select("*");
   if (error) throw new Error(error.message);
