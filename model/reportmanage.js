00e4fa93200b7f4c623ae67e2711fff0971546e5
himalf <himal.fullel15@gmail.com>
2024-10-21 21:17:12 +0545

    supabase key generatrion

--- model/reportmanage.js
+++ model/reportmanage.js
@@ -2,7 +2,7 @@ const { createClient } = require("@supabase/supabase-js");
 const supabaseURL = process.env.SUPABASE_URL;
 const supabaseKEY = process.env.SUPABASE_KEY;
 // console.log(supabaseURL, "urlof supabse");
-console.log(supabaseKEY, "key of supabase");
+// console.log(supabaseKEY, "key of supabase");
 const supabase = createClient(supabaseURL, supabaseKEY);
 // console.log(supabase, "hello");
 const getReports = async () => {
